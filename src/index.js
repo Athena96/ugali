a99d0a772a1ff9e12764f41c4baeb667dcb418a5
Jared Franzone <jaredfranzone@gmail.com>
2020-05-12 19:49:41 -0700

    code improvements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import AppWithAuth from './components/AppWithAuth'
 import * as serviceWorker from './serviceWorker';
 
